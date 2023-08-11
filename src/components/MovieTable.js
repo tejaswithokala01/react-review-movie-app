f16f95dc044cb809d1f202fe75a8fca6a724382a
tejaswithokala01 <142023588+tejaswithokala01@users.noreply.github.com>
2023-08-11 22:18:18 +0530

    Update MovieTable.js

--- src/components/MovieTable.js
+++ src/components/MovieTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,31 +8,86 @@ import {
   TableRow,
   Paper,
   Button,
+  TableSortLabel,
+  Box,
 } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 
 import { formatReview } from "../helpers";
 
 const columns = [
-  { id: "title", label: "Title", minWidth: 170 },
-  { id: "reviews", label: "Review Score", minWidth: 100 },
+  { id: "title", label: "Title", minWidth: 70,  },
+  { id: "reviews", label: "Review Score", minWidth: 10 },
   {
     id: "company",
     label: "Company",
-    minWidth: 170,
+    minWidth: 17,
   },
   { id: "button", label: "Review Movie" },
 ];
 
-const MovieTable = ({ movies = [], setOpenModal, selectedRow, setSelectedRow }) => {
-    
-    const handleRowSelect = (id) => {
-        if (selectedRow.includes(id)) {
-          setSelectedRow(selectedRow.filter((rowId) => rowId !== id));
-        } else {
-          setSelectedRow([id]);
-          setOpenModal(true);
-        }
-      };
+const MovieTable = ({
+  movies = [],
+  setOpenModal,
+  selectedRow,
+  setSelectedRow,
+}) => {
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("reviews");
+  const [page] = React.useState(0);
+  const [rowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  const getComparator = useCallback((order, orderBy) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }, [])
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(movies, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [order, orderBy, page, rowsPerPage, getComparator, movies]
+  );
+
+  const handleRowSelect = (id) => {
+    if (selectedRow.includes(id)) {
+      setSelectedRow(selectedRow.filter((rowId) => rowId !== id));
+    } else {
+      setSelectedRow([id]);
+      setOpenModal(true);
+    }
+  };
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -46,13 +101,26 @@ const MovieTable = ({ movies = [], setOpenModal, selectedRow, setSelectedRow }) 
                   align={column.align}
                   style={{ minWidth: column.minWidth }}
                 >
-                  {column.label}
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={orderBy === column.id ? order : "asc"}
+                    onClick={(event) => handleRequestSort(event, column.id)}
+                  >
+                    {column.label}
+                    {orderBy === column.id ? (
+                      <Box component="span" sx={visuallyHidden}>
+                        {order === "desc"
+                          ? "sorted descending"
+                          : "sorted ascending"}
+                      </Box>
+                    ) : null}
+                  </TableSortLabel>
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {movies.map((item) => (
+            {visibleRows.map((item) => (
               <TableRow hover key={item.id}>
                 <TableCell>{item.title}</TableCell>
                 <TableCell>{formatReview(item.reviews)}</TableCell>
